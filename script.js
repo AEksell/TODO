ac716bd5ffc5276fcebc409ad8b3cf793829f49c
AEksel <akselgab@gmail.com>
2023-10-24 12:49:32 +0200

    Optimization
    Made better changes to Rescalability and accessibility
    
    Created Buttons to have functionality be added later

---
+++ script.js
@@ -0,0 +1,19 @@
+function New()
+{
+    console.log("Making new Note")
+}
+
+function Open()
+{
+    console.log("Open Note")
+}
+
+function Save()
+{
+    console.log("Save Note")
+}
+
+function Destroy()
+{
+    console.log("Destroy Note")
+}
