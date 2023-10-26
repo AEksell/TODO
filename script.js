69131ccc806a4b9617c1f6233abf1c875bb7a721
AEksel <akselgab@gmail.com>
2023-10-26 10:38:33 +0200

    Add object button
    Button to add list of tasks you can write in.
    
    Main div height changes depending on how many tasks you have
    
    #6

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// ALL VARIABLES
+const MainDiv = document.getElementById("MainDiv");
+
+let AddButton = document.getElementById("AddTask");
+
+ const TaskDiv = document.getElementById("TaskDiv");
+ const TaskList = document.getElementById("TaskList")
+        
+
+let numberOfTasks = 0
+const SizeIncrease = 80;
+// ALL FUNCTIONS
 function New()
 {
     console.log("Making new Note")
@@ -11,6 +23,40 @@ function Open()
 function Save()
 {
     console.log("Save Note")
+}
+
+
+function AddTask()
+{   // I NEED THESE VARIABLES IN BOTH ADDTASK AND REMOVETASK FOR IT TO BE LOCALLY SAVED AND BE REPEATING
+    // counter of tasks
+    numberOfTasks ++;
+
+   //Adds the actual Text to edit your task
+    const taskInput = document.createElement("li");
+   
+ TaskList.appendChild(taskInput);
+    taskInput.classList.add("TaskText");
+    taskInput.innerHTML = "Type here to edit!";
+    taskInput.contentEditable = true;
+    MainDiv.appendChild(TaskDiv);
+
+    taskInput.focus();
+
+    // Code that changes height of MainDiv
+    const MainDivCurrentHeight = window.getComputedStyle(MainDiv).height;
+    const MainDivCurrentHeightValue = parseInt(MainDivCurrentHeight)
+    let ExtraHeight = MainDivCurrentHeightValue + SizeIncrease;
+
+    if (numberOfTasks > 6)
+    {
+        MainDiv.style.height = ExtraHeight + `px`;
+    }
+
+}   
+
+function RemoveTask()
+{   // I NEED THESE VARIABLES IN BOTH ADDTASK AND REMOVETASK FOR IT TO BE LOCALLY SAVED AND BE REPEATING
+
 }
 
 function Destroy()
